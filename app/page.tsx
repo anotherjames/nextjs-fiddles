a0ed91bbd02fca9e900c7a42d43c558a1d2a7c7e
James Williams <james.williams@computerminds.co.uk>
2025-05-30 11:12:24 +0100

    i did the thinhng

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { Suspense } from 'react';
 
 export default async function Page() {
   return (
-    <main>
+    <main>pooooooop
       <h1 className={`mb-4 text-9xl text-pink-300`}>
-        FPL data; hi Ben!
+        FPL data; hi Ben!hfbsfbfsbh
       </h1>
-      <Suspense fallback={<span>Loading...</span>}>
+      <Suspense fallback={<span>cardyou are pooooooooooooooooooooooooooooooooooooooooooooo.</span>}>
         <FplTable></FplTable>
       </Suspense>
     </main>
