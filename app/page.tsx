8449534c6457bf71632752bcdd166766e7896fdb
James Williams <james.williams@computerminds.co.uk>
2025-05-30 10:59:51 +0100

    Ben's first coding

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { Suspense } from 'react';
 export default async function Page() {
   return (
     <main>
-      <h1 className={`mb-4 text-xl md:text-2xl`}>
-        FPL data
+      <h1 className={`mb-4 text-xl md:text-2xl text-pink`}>
+        FPL data; hi Ben!
       </h1>
       <Suspense fallback={<span>Loading...</span>}>
         <FplTable></FplTable>
